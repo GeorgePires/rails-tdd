b7499f9f2a16ed8d1b50b5d5e82cd2ab18f38be5
GeorgePires <georgeasp.pi@gmail.com>
2022-11-25 11:29:01 -0300

    :test_tube: RSpec fundamentals

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,14 +1,20 @@
+# frozen_string_literal: true
+
+# * SETUP
+# * EXERCISE
+# * VERIFY
+
 require 'calculator'
 
 describe Calculator do
   it 'sum 2 numbers' do
-    calc = Calculator.new
+    calc = described_class.new
     result = calc.sum(2, 3)
     expect(result).to eq(5)
   end
 
   it 'multiply 2 numbers' do
-    calc = Calculator.new
+    calc = described_class.new
     result = calc.multiply(2, 3)
     expect(result).to eq(6)
   end
